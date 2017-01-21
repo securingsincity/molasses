cb7b0455350d6015ac02b3edbe418678f0cf1bb6
James Hrisho <james.hrisho@gmail.com>
2017-01-20 22:08:08 -0500

    Reduce loading errors

--- lib/adapters/mongo.ex
+++ lib/adapters/mongo.ex
@@ -1,96 +1,98 @@
-defmodule Molasses.StorageAdapter.MongoDB do
-    @moduledoc """
-    Storage Adapter for mongodb for use in molasses
-    """
-  alias Molasses.Util
+if Code.ensure_loaded?(Mongo) do
+    defmodule Molasses.StorageAdapter.MongoDB do
+        @moduledoc """
+        Storage Adapter for mongodb for use in molasses
+        """
+    alias Molasses.Util
 
-  def get_features(client) do
-    cursor = Mongo.find(client, "feature_toggles", %{})
-    Enum.to_list(cursor)
-  end
-
-  def get(client, key) do
-    cursor = Mongo.find(client, "feature_toggles", %{name: key})
-    case Enum.at(cursor,0) do
-        0 -> nil
-        result -> result
+    def get_features(client) do
+        cursor = Mongo.find(client, "feature_toggles", %{})
+        Enum.to_list(cursor)
     end
-  end
 
-  def set(client, key, value) do
-    Mongo.update_one!(client,
-    "feature_toggles",
-    %{"name": key},
-    %{"$set": Map.merge(value, %{name: key})}, [upsert: true]
-  )
-  end
-
-  def remove(client, key) do
-    Mongo.delete_one(client, "feature_toggles", %{"name": key})
-  end
-
-  def activate(client, key) do
-    set(client, key, %{
-        name: key,
-        percentage: 100,
-        active: true
-    })
-  end
-
-  def activate(client, key, percentage) when is_integer(percentage) do
-    set(client, key, %{
-        name: key,
-        percentage: percentage,
-        active: true
-    })
-  end
-
-  def activate(client, key, users) when is_list(users) do
-    activated_users = Enum.join(users,",")
-    set(client, key, %{
-        name: key,
-        percentage: 100,
-        active: true,
-        users: activated_users
-    })
-  end
-
-  def activate(client, key, group) do
-    set(client, key, %{
-        name: key,
-        percentage: 100,
-        active: true,
-        users: group
-    })
-  end
-
-  def deactivate(client, key) do
-    set(client, key, %{
-        name: key,
-        percentage: 0,
-        active: false,
-        users: ""
-    })
-  end
-
-  def get_feature(repo, key) do
-    case get(repo, key) do
-        nil -> {:error, "failure"}
-        %{"name"=> key, "active"=> active, "percentage"=> percentage, "users"=> users} ->
-            %{
-                name: key,
-                active: active,
-                percentage: percentage,
-                users: Util.convert_to_list(users),
-            }
-        %{"name"=> key, "active"=> active, "percentage"=> percentage} ->
-            %{
-                name: key,
-                active: active,
-                percentage: percentage,
-                users: [],
-            }
+    def get(client, key) do
+        cursor = Mongo.find(client, "feature_toggles", %{name: key})
+        case Enum.at(cursor,0) do
+            0 -> nil
+            result -> result
+        end
     end
-  end
 
+    def set(client, key, value) do
+        Mongo.update_one!(client,
+        "feature_toggles",
+        %{"name": key},
+        %{"$set": Map.merge(value, %{name: key})}, [upsert: true]
+    )
+    end
+
+    def remove(client, key) do
+        Mongo.delete_one(client, "feature_toggles", %{"name": key})
+    end
+
+    def activate(client, key) do
+        set(client, key, %{
+            name: key,
+            percentage: 100,
+            active: true
+        })
+    end
+
+    def activate(client, key, percentage) when is_integer(percentage) do
+        set(client, key, %{
+            name: key,
+            percentage: percentage,
+            active: true
+        })
+    end
+
+    def activate(client, key, users) when is_list(users) do
+        activated_users = Enum.join(users,",")
+        set(client, key, %{
+            name: key,
+            percentage: 100,
+            active: true,
+            users: activated_users
+        })
+    end
+
+    def activate(client, key, group) do
+        set(client, key, %{
+            name: key,
+            percentage: 100,
+            active: true,
+            users: group
+        })
+    end
+
+    def deactivate(client, key) do
+        set(client, key, %{
+            name: key,
+            percentage: 0,
+            active: false,
+            users: ""
+        })
+    end
+
+    def get_feature(repo, key) do
+        case get(repo, key) do
+            nil -> {:error, "failure"}
+            %{"name"=> key, "active"=> active, "percentage"=> percentage, "users"=> users} ->
+                %{
+                    name: key,
+                    active: active,
+                    percentage: percentage,
+                    users: Util.convert_to_list(users),
+                }
+            %{"name"=> key, "active"=> active, "percentage"=> percentage} ->
+                %{
+                    name: key,
+                    active: active,
+                    percentage: percentage,
+                    users: [],
+                }
+        end
+    end
+
+    end
 end
--- lib/adapters/redis.ex
+++ lib/adapters/redis.ex
@@ -1,61 +1,63 @@
-defmodule Molasses.StorageAdapter.Redis do
-  @moduledoc """
-    Storage Adapter for redis for use in molasses
-    """
-  alias Molasses.Util
+if Code.ensure_loaded?(Exredis) do
+  defmodule Molasses.StorageAdapter.Redis do
+    @moduledoc """
+      Storage Adapter for redis for use in molasses
+      """
+    alias Molasses.Util
 
-  def get_features(client) do
-    keys = Exredis.Api.keys "molasses_*"
-    Enum.map(keys, fn(x) ->
-      formatted = String.replace(x,"molasses_", "")
-      get_feature(client, formatted)
-    end)
-  end
-  def get(client, key) do
-    Exredis.Api.get client, "molasses_#{key}"
-  end
-
-  def set(client, key, value) do
-    Exredis.Api.set client,  "molasses_#{key}", value
-  end
-
-  def remove(client, key) do
-    Exredis.Api.del client,  "molasses_#{key}"
-  end
-
-  def activate(client, key) do
-    set(client, key, "true|100|")
-  end
-
-  def activate(client, key, percentage) when is_integer(percentage) do
-    set(client, key, "true|#{percentage}|")
-  end
-
-  def activate(client, key, users) when is_list(users) do
-    activated_users = Enum.join(users,",")
-    set(client, key, "true|100|#{activated_users}")
-  end
-
-  def activate(client, key, group) do
-    set(client, key, "true|100|#{group}")
-  end
-
-  def deactivate(client, key) do
-    set(client, key, "false|0|")
-  end
-
-  def get_feature(client, key) do
-    case get(client, key) do
-      :undefined -> {:error, "failure"}
-      result ->
-        [active, percentage, users] = String.split(result, "|")
-        %{
-          name: key,
-          active: Util.return_bool(active),
-          percentage: String.to_integer(percentage),
-          users: Util.convert_to_list(users),
-        }
+    def get_features(client) do
+      keys = Exredis.Api.keys "molasses_*"
+      Enum.map(keys, fn(x) ->
+        formatted = String.replace(x,"molasses_", "")
+        get_feature(client, formatted)
+      end)
     end
-  end
+    def get(client, key) do
+      Exredis.Api.get client, "molasses_#{key}"
+    end
 
+    def set(client, key, value) do
+      Exredis.Api.set client,  "molasses_#{key}", value
+    end
+
+    def remove(client, key) do
+      Exredis.Api.del client,  "molasses_#{key}"
+    end
+
+    def activate(client, key) do
+      set(client, key, "true|100|")
+    end
+
+    def activate(client, key, percentage) when is_integer(percentage) do
+      set(client, key, "true|#{percentage}|")
+    end
+
+    def activate(client, key, users) when is_list(users) do
+      activated_users = Enum.join(users,",")
+      set(client, key, "true|100|#{activated_users}")
+    end
+
+    def activate(client, key, group) do
+      set(client, key, "true|100|#{group}")
+    end
+
+    def deactivate(client, key) do
+      set(client, key, "false|0|")
+    end
+
+    def get_feature(client, key) do
+      case get(client, key) do
+        :undefined -> {:error, "failure"}
+        result ->
+          [active, percentage, users] = String.split(result, "|")
+          %{
+            name: key,
+            active: Util.return_bool(active),
+            percentage: String.to_integer(percentage),
+            users: Util.convert_to_list(users),
+          }
+      end
+    end
+
+  end
 end
