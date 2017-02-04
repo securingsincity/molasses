46f9c60e75f78158040859031f2007c73ba07780
James Hrisho <james.hrisho@gmail.com>
2017-02-03 22:45:01 -0500

    adds api endpoint support

---
+++ lib/router.ex
@@ -0,0 +1,20 @@
+if Code.ensure_loaded?(Phoenix) do
+    defmodule Molasses.Router do
+        use Phoenix.Router
+        defmacro __using__(_opts \\ []) do
+            quote do
+                import unquote(__MODULE__)
+            end
+        end
+
+        defmacro api_routes(_opts \\ []) do
+            quote do
+                get "/:name/:user_id", Api.ApiController, :is_active
+                put "/:name", Api.ApiController, :update
+                post "/:name", Api.ApiController, :update
+                post "/", Api.ApiController, :create
+                get "/", Api.ApiController, :index
+            end
+        end
+    end
+end
